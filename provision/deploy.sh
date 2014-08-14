a6133d09dc9aa091b235b58718aa093a15c0927e
Benjamin Mooney <nejibem@gmail.com>
2014-08-14 18:35:30 +1000

    tidyup deploy script

--- provision/deploy.sh
+++ provision/deploy.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-#ansible-playbook provision/deploy.yml -i provision/hosts --private-key=$HOME/.vagrant.d/insecure_private_key
-#ansible all -i vagrant_ansible_inventory -u vagrant --private-key ~/.vagrant.d/insecure_private_key -m ping
-
 ansible-playbook -i provision/hosts -u vagrant provision/deploy.yml
